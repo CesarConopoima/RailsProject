e6817ce9a92460226fe08e33fab46645a460c35f
CesarConopoima <cesar.conopoima@gmail.com>
2014-02-04 14:23:29 -0430

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -4,9 +4,9 @@ require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  #Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+   Bundler.require(:default, :assets, Rails.env)
 end
 module Aplicacion
   class Application < Rails::Application
@@ -51,6 +51,7 @@ module Aplicacion
     # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
+    #config.assets.initialize_on_precompile = false
     config.assets.enabled = true
    
 
