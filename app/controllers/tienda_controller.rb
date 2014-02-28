38c18aba3be942c12c1a061747bf036e2404d718
CesarConpoima <cesar.conopoima@gmail.com>
2014-02-27 20:56:19 -0430

    miscelaneus y arreglos varios

--- app/controllers/tienda_controller.rb
+++ app/controllers/tienda_controller.rb
@@ -9,6 +9,7 @@ class TiendaController < ApplicationController
     @productoTran=Producto.find_by_sql("select nombre AS nombres from productos where marca = 'Trane' group by nombres order by nombres")
     @productoYork=Producto.find_by_sql("select nombre AS nombres from productos where marca = 'York' group by nombres order by nombres")
     @cart = current_cart
+    @productoMis = Producto.find_by_sql("select split_part(nombre,' ', 1) AS nombres,count(*) AS number from products where marca like '#{@marca}' group by nombres having count(*) <= 6") 
   end
 
   def pagina
@@ -20,6 +21,7 @@ class TiendaController < ApplicationController
     @marca = params[:marca].downcase
     @productos1 = Producto.find_by_sql("select marca from productos group by marca order by marca")
     @productos2 = Producto.find_by_sql("select * from productos where marca like '#{@marca}' group by SUBSTR(nombre,0,4)")
+    @productoMis = Producto.find_by_sql("select split_part(nombre,' ', 1) AS nombres,count(*) AS number from products where marca like '#{@marca}' group by nombres having count(*) <= 6")  
   end
 
   def vista
@@ -32,6 +34,7 @@ class TiendaController < ApplicationController
     @marca = params[:marca].downcase.capitalize
     @nombre = params[:nombre].split(" ").first.upcase 
     @productos2 = Producto.find_by_sql("select * from productos where marca like '#{@marca}' and nombre like '#{@nombre}%'")
+    @productoMis = Producto.find_by_sql("select split_part(nombre,' ', 1) AS nombres,count(*) AS number from products where marca like '#{@marca}' group by nombres having count(*) <= 6") 
   end 
 
   def miscelaneus
@@ -48,17 +51,11 @@ class TiendaController < ApplicationController
   def vistaDetalle
     @cart = current_cart    
     @productos = Producto.search(params[:search])
-    @productos1= Producto.marcas
-    @productoCop=Producto.find_by_sql("select nombre from (select nombre,count() 
-            AS number from productos 
-            where marca = 'Copeland' group by SUBSTR(nombre,0,4))
-            where number > 6")
-    @productoCarr=Producto.find_by_sql("select nombre from (select nombre,count() 
-            AS number from productos 
-            where marca = 'Carrier' group by SUBSTR(nombre,0,4))
-            where number > 6")
-    @productoTran=Producto.find_by_sql("select nombre from productos where marca = 'Trane' group by nombre")
-    @productoYork=Producto.find_by_sql("select nombre from productos where marca = 'York' group by nombre")
+    @productos1 = Producto.find_by_sql("select marca from productos group by marca order by marca")
+    @productoCop=Producto.find_by_sql("select split_part(nombre,' ', 1) AS nombres,count(*) AS number from productos where marca = 'Copeland' group by nombres having count(*) > 6 order by nombres")
+    @productoCarr=Producto.find_by_sql("select split_part(nombre,' ', 1) AS nombres,count(*) AS number from productos where marca = 'Carrier' group by nombres having count(*) > 6 order by nombres")
+    @productoTran=Producto.find_by_sql("select nombre AS nombres from productos where marca = 'Trane' group by nombres order by nombres")
+    @productoYork=Producto.find_by_sql("select nombre AS nombres from productos where marca = 'York' group by nombres order by nombres")
     @marca = params[:marca].downcase.capitalize
     @nombre = params[:nombre]
     @modelo = params[:modelo].split("/").first.to_s
