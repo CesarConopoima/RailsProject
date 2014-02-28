a14160c464b661565deee08265dc955236b1085b
CesarConpoima <cesar.conopoima@gmail.com>
2014-02-27 21:22:11 -0430

    agregar otra vista

--- app/controllers/tienda_controller.rb
+++ app/controllers/tienda_controller.rb
@@ -18,9 +18,9 @@ class TiendaController < ApplicationController
     @productoCarr=Producto.find_by_sql("select split_part(nombre,' ', 1) AS nombres,count(*) AS number from productos where marca = 'Carrier' group by nombres having count(*) > 6 order by nombres")
     @productoTran=Producto.find_by_sql("select nombre AS nombres from productos where marca = 'Trane' group by nombres order by nombres")
     @productoYork=Producto.find_by_sql("select nombre AS nombres from productos where marca = 'York' group by nombres order by nombres")
-    @marca = params[:marca].downcase
+    @marca = params[:marca].downcase.capitalize
     @productos1 = Producto.find_by_sql("select marca from productos group by marca order by marca")
-    @productos2 = Producto.find_by_sql("select * from productos where marca like '#{@marca}' group by SUBSTR(nombre,0,4)")
+    @productos2 = Producto.find_by_sql("select split_part(nombre,' ', 1) AS nombres,count(*) AS number from productos where marca like '#{@marca}' group by nombres order by nombres")
     @productoMis = Producto.find_by_sql("select split_part(nombre,' ', 1) AS nombres,count(*) AS number from productos where marca like '#{@marca}' group by nombres having count(*) <= 6")  
   end
 
