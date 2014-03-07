39e7a77afb0302bedb10b94866640fc3a8bfe456
CesarConpoima <cesar.conopoima@gmail.com>
2014-03-06 21:17:52 -0430

    cambios en validacion

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,11 @@
 class Order < ActiveRecord::Base
 	has_many :line_items, dependent: :destroy
-	PAYMENT_TYPES = [ "Deposito Bancario" , "Tarjeta de Credito", "Cheque" ]
-	validates :nombre, :empresa, :direccion, :email, :telefono, presence: true
+	PAYMENT_TYPES = [ "Deposito Bancario" , "Transferencia Bancaria","Pago en la tienda"]
+	validates_presence_of :nombre, message => "Esta casilla no puede estar en blanco" 
+	validates_presence_of :empresa, message => "Esta casilla no puede estar en blanco"
+	validates_presence_of :direccion, message => "Esta casilla no puede estar en blanco" 
+	validates_presence_of :email, message => "Esta casilla no puede estar en blanco" 
+	validates_presence_of :telefono, message => "Esta casilla no puede estar en blanco"  
 	validates :tipoPago, inclusion: PAYMENT_TYPES
 	def add_line_items_from_cart(cart)
 		cart.line_items.each do |item|
