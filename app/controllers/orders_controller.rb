a8049f23369c8a45ce66bfdfbed112b9056d74cd
CesarConpoima <cesar.conopoima@gmail.com>
2014-03-12 20:35:03 -0430

    revertir y probar de nuevo en ordenes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -39,7 +39,6 @@ class OrdersController < ApplicationController
     end
     
     @order = Order.new
-
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @order }
