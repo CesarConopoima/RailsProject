7ac2b4b134cc89dab3622a0c401e210ef4b16d75
CesarConpoima <cesar.conopoima@gmail.com>
2014-03-12 21:30:29 -0430

    reparaciones

--- db/migrate/20140313011623_add_direccion2_to_orders.rb
+++ db/migrate/20140313011623_add_direccion2_to_orders.rb
@@ -1,7 +1,7 @@
 class AddDireccion2ToOrders < ActiveRecord::Migration
   def change
-    add_column :orders, :direccion2, :text
-    add_column :orders, :telefono2, :string
+    add_column :orders, :direccionaux, :text
+    add_column :orders, :telefonoaux, :string
     add_column :orders, :tipoenvio, :string
     add_column :orders, :localidadvenezuela, :string
   end
