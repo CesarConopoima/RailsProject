9f66de5c2f0c8a0f8549ee7dd1a827575bb82dd5
CesarConpoima <cesar.conopoima@gmail.com>
2014-03-18 23:08:07 -0430

    agregar barra de busqueda

--- app/controllers/tienda_controller.rb
+++ app/controllers/tienda_controller.rb
@@ -16,7 +16,7 @@ class TiendaController < ApplicationController
     @cart = current_cart
     @productoCop=Producto.find_by_sql("select split_part(nombre,' ', 1) AS nombres,count(*) AS number from productos where marca = 'Copeland' group by nombres having count(*) > 6 order by nombres")
     @productoCarr=Producto.find_by_sql("select split_part(nombre,' ', 1) AS nombres,count(*) AS number from productos where marca = 'Carrier' group by nombres having count(*) > 6 order by nombres")
-   @productoTran=Producto.find_by_sql("select split_part(nombre,' ', 1) AS nombres,count(*) AS number from productos where marca = 'Trane' group by nombres order by nombres")
+    @productoTran=Producto.find_by_sql("select split_part(nombre,' ', 1) AS nombres,count(*) AS number from productos where marca = 'Trane' group by nombres order by nombres")
     @productoYork=Producto.find_by_sql("select split_part(nombre,' ', 1) AS nombres,count(*) AS number from productos where marca = 'York' group by nombres order by nombres")
     @marca = params[:marca].downcase.capitalize
     @productos1 = Producto.find_by_sql("select marca from productos group by marca order by marca")
