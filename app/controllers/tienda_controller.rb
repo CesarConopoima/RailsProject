6422c73739930c254aee5076211078315d919ba6
CesarConpoima <cesar.conopoima@gmail.com>
2014-04-19 10:16:33 -0430

    repairQuery

--- app/controllers/tienda_controller.rb
+++ app/controllers/tienda_controller.rb
@@ -20,7 +20,7 @@ class TiendaController < ApplicationController
     @productoYork=Producto.find_by_sql("select split_part(nombre,' ', 1) AS nombres,count(*) AS number from productos where marca = 'York' group by nombres order by nombres")
     @marca = params[:marca].downcase.capitalize
     @productos1 = Producto.find_by_sql("select marca from productos group by marca order by marca")
-    @productos2 = Producto.find_by_sql("select split_part(nombre,' ', 1) AS nombres,count(*) AS number from productos where marca like '#{@marca}' group by nombres order by nombres")
+    @productos2 = Producto.find_by_sql("select split_part(nombre,' ', 1) AS nombres,modelo,count(*) AS number from productos where marca like '#{@marca}' group by nombres order by nombres")
     @productoMis = Producto.find_by_sql("select split_part(nombre,' ', 1) AS nombres,count(*) AS number from productos where marca like '#{@marca}' group by nombres having count(*) <= 2")  
   end
 
