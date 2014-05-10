f5c6ca461984f86563e24cb8f898d82e65fc74d9
CesarConpoima <cesar.conopoima@gmail.com>
2014-05-10 17:25:20 -0430

    cambioVistaServicios

--- app/assets/javascripts/code.js
+++ app/assets/javascripts/code.js
@@ -15,25 +15,6 @@ $(document).ready(ready);
 $(document).on('page:load', ready);
 
 var ready = function() {
-	var images = [ "/assets/SlideShow/BombasCarrier.jpg", "/assets/SlideShow/KitDeEmpacaduras.jpg", "/assets/SlideShow/EnsambladosCopeland.jpg"];
-    var currentImage = 0;
-    function changeBackground() {
-        $( '[class="slideShow"]' ).css( 
-        	{ content: 'url(' + images[ ++currentImage ] + ')'}
-        	);
-        if ( currentImage >= images.length - 1 ) {
-            currentImage -= images.length;
-        }
-    }
-    setInterval( changeBackground, 5000 );  
-};
-
-$(document).ready(ready);
-$(document).on('page:load', ready);
-
-
-
-var ready = function() {
 	$('[class="Proc1"]').hide();
 	$('[class="Proc2"]').hide();
 	$('[class="Proc3"]').hide();
@@ -42,19 +23,27 @@ var ready = function() {
 	$('[class="Proc6"]').hide();
 	$('[class="Proc7"]').hide();
 	$('[class="Proc8"]').hide();
-	$('.servicio img').click(function() {
-		var clase = $(this).attr('class').replace("img","");
-		$('[class="default"]').hide(600,"easeInExpo");
+	$('.servicio h3').click(function() {
+	$('[class="Proc1"]').hide(1000,"easeInExpo");
+	$('[class="Proc2"]').hide(1000,"easeInExpo");
+	$('[class="Proc3"]').hide(1000,"easeInExpo");
+	$('[class="Proc4"]').hide(1000,"easeInExpo");
+	$('[class="Proc5"]').hide(1000,"easeInExpo");
+	$('[class="Proc6"]').hide(1000,"easeInExpo");
+	$('[class="Proc7"]').hide(1000,"easeInExpo");
+	$('[class="Proc8"]').hide(1000,"easeInExpo");
+		var clase = $(this).attr('class').replace("Tit","");
 		$('[class="'+clase+'"]').show(1000,"easeInExpo");
 	});
-	$('[class="button"]').click(function(){
+	$('[class="button"]').click(function() {
 		var clase = $(this).parent().prop('className');
-		$('[class="'+clase+'"]').hide(600,"easeInExpo");
+		$('[class="'+clase+'"]').hide(1000,"easeInExpo");
 	});
 };
 
 $(document).ready(ready);
 $(document).on('page:load', ready);
+
 
 var ready = function() {
 	$('[class="imgComp1"]').mouseenter(function() {
