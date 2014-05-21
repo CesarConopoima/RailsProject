98c45f53d7823b3f3244b447e5b8d64d22cde48f
CesarConpoima <cesar.conopoima@gmail.com>
2014-05-21 11:09:34 -0430

    validationsForOrdersAndUsers

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ActiveRecord::Base
   VENEZUELA_ESTADOS = [ "Anzoategui","Amazonas","Apure","Aragua","Barinas","Bolivar","Carabobo","Cojedes","Delta Amacuro","Distrito Capital","Falcon","Guarico","Lara","Merida","Miranda","Monagas","Nueva Esparta","Portuguesa","Tachira","Trujillo","Vargas","Yaracuy","Zulia"]
   before_save :setup_role
   has_and_belongs_to_many :roles
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,:presence => { :message => "Este campo debe ser llenado" },format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :nombre,:telefono,:localidadVenezuela,:apellido,
-  :nombreEmpresa,:rifEmpresa,:direccion,:codigoPostal,
-  :email, :presence => { :message => "Este campo debe ser llenado" }
+  :nombreEmpresa,:rifEmpresa,:direccion,
+  :codigoPostal, :presence => { :message => "Este campo debe ser llenado" }
   validates :localidadVenezuela, inclusion: VENEZUELA_ESTADOS
   
   devise :database_authenticatable, :registerable,
