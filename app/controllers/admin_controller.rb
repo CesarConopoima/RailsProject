2decef074f5474ee061918fc100aa352dabb6d2c
CesarConpoima <cesar.conopoima@gmail.com>
2014-08-22 18:13:50 -0430

    moreRestrictionsInAdminZone

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,7 @@
 class AdminController < ApplicationController
-  before_filter :authorize
+  #before_filter :authorize
+  before_filter :authenticate_user!
+  load_and_authorize_resource
   def index
   	@total_orders = Order.count
   end
