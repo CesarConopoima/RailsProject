609d638bd36c7dcfd6805914b5a9d753ee191644
CesarConpoima <cesar.conopoima@gmail.com>
2014-10-29 22:23:51 -0430

    mailer

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -29,8 +29,8 @@ class OrderNotifier < ActionMailer::Base
   def paymentInformation(order)
     @order = order
     @numero = @order.id
-    mail :to => "copelancitadevenezuela@gmail.com", subject: 'Han Pagado la orden de referencia #{@numero}'
-    mail :to => "cesar.conopoima@gmail.com", subject: 'Han Pagado la orden de referencia #{@numero}'
+    mail :to => "copelancitadevenezuela@gmail.com", subject: 'Hemos recibido un pedido'
+    mail :to => "cesar.conopoima@gmail.com", subject: 'Hemos recibido un pedido'
   end
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
